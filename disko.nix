4524069b371660fb152652e8d8e8b28ad84499a1
thejondaw <mrjondaw@gmail.com>
2024-12-19 23:46:23 -0600

    12/19/24 #6

--- disko.nix
+++ disko.nix
@@ -15,7 +15,7 @@
                type = "filesystem";
                format = "vfat";
                mountpoint = "/boot";
-               mountOptions = ["umask=0077"];
+               mountOptions = [ "umask=0077" ];
              };
            };
 
